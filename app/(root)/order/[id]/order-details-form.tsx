a0ff9da08a703523d143be8ed4ee4d4e3cb3e155
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-06-20 19:01:45 +0700

    Pay order by paypal

--- app/(root)/order/[id]/order-details-form.tsx
+++ app/(root)/order/[id]/order-details-form.tsx
@@ -2,10 +2,17 @@
 
 import Link from 'next/link'
 import Image from 'next/image'
+import {
+  PayPalButtons,
+  PayPalScriptProvider,
+  usePayPalScriptReducer,
+} from '@paypal/react-paypal-js'
 
+import { Order } from '@/types'
 import { formatCurrency, formatDateTime, formatId } from '@/lib/utils'
 import { Badge } from '@/components/ui/badge'
 import { Card, CardContent } from '@/components/ui/card'
+import { useToast } from '@/components/ui/use-toast'
 import {
   Table,
   TableBody,
@@ -14,10 +21,19 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
-import { Order } from '@/types'
-import { useToast } from '@/components/ui/use-toast'
 
-const OrderDetailsForm = ({ order }: { order: Order }) => {
+import {
+  approvePayPalOrder,
+  createPayPalOrder,
+} from '@/lib/actions/order.actions'
+
+const OrderDetailsForm = ({
+  order,
+  paypalClientId,
+}: {
+  order: Order
+  paypalCientId: string
+}) => {
   const { toast } = useToast()
   const {
     shippingAddress,
@@ -32,6 +48,35 @@ const OrderDetailsForm = ({ order }: { order: Order }) => {
     isDelivered,
     deliveredAt,
   } = order
+
+  const PrintLoadingState = () => {
+    const [{ isPending, isRejected }] = usePayPalScriptReducer()
+    let status = ''
+    if (isPending) {
+      status = 'Loading PayPal...'
+    } else if (isRejected) {
+      status = 'Error in loading PayPal.'
+    }
+    return status
+  }
+
+  const handleCreatePayPalOrder = async () => {
+    const res = await createPayPalOrder(order.id)
+    if (!res.success)
+      return toast({
+        description: res.message,
+        variant: 'destructive',
+      })
+    return res.data
+  }
+
+  const handleApprovePayPalOrder = async (data: { orderID: string }) => {
+    const res = await approvePayPalOrder(order.id, data)
+    toast({
+      description: res.message,
+      variant: res.success ? 'default' : 'destructive',
+    })
+  }
 
   return (
     <>
@@ -138,6 +183,18 @@ const OrderDetailsForm = ({ order }: { order: Order }) => {
                 <div>Total</div>
                 <div>{formatCurrency(totalPrice)}</div>
               </div>
+
+              {!isPaid && paymentMethod === 'PayPal' && (
+                <div>
+                  <PayPalScriptProvider options={{ clientId: paypalClientId }}>
+                    <PrintLoadingState />
+                    <PayPalButtons
+                      createOrder={handleCreatePayPalOrder}
+                      onApprove={handleApprovePayPalOrder}
+                    />
+                  </PayPalScriptProvider>
+                </div>
+              )}
             </CardContent>
           </Card>
         </div>
--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -1,11 +1,12 @@
 import { notFound } from 'next/navigation'
 
 import OrderDetailsForm from './order-details-form'
-import { getOrderById } from '@/lib/actions/order.actions'
 import { auth } from '@/auth'
+import { getOrderById } from '@/lib/actions/order.actions'
+import { APP_NAME } from '@/lib/constants'
 
 export const metadata = {
-  title: 'Order Details',
+  title: ` - ${APP_NAME}`,
 }
 
 const OrderDetailsPage = async ({
@@ -19,7 +20,12 @@ const OrderDetailsPage = async ({
   if (!order) notFound()
 
   const session = await auth()
-  return <OrderDetailsForm order={order} />
+  return (
+    <OrderDetailsForm
+      order={order}
+      paypalClientId={process.env.PAYPAL_CLIENT_ID || 'sb'}
+    />
+  )
 }
 
 export default OrderDetailsPage
